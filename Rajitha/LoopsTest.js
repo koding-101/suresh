6b908af9f49019eaebf6a36ad915f4db94d5acfe
Rajitha Tupakula <rtupaku>
2019-12-07 22:49:38 -0800

    Added few more tests

--- Rajitha/LoopsTest.js
+++ Rajitha/LoopsTest.js
@@ -9,8 +9,8 @@ const arr = [1,2,3,4,5];
 //Return Odd Numbers
 function oddNumbers(arr) {
     let oddArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 != 0) {
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i]%2 != 0) {
             oddArray.push(arr[i]);
         }
     }
@@ -21,8 +21,8 @@ console.log(oddNumbers(arr));
 //Return Even Numbers
 function evenNumbers(arr){
     let evenArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 == 0) {
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i]%2 == 0) {
             evenArray.push(arr[i]);
         }
     }
@@ -54,7 +54,7 @@ console.log(combineArray(arr,arr1));
 
 const alternateArray = (arr,arr1) => {
     let altArray = [];
-    for (let i = 0; i < arr.length; i++) {
+    for (let i=0; i<arr.length; i++) {
         altArray = altArray.concat(arr[i], arr1[i]);
     }
     return altArray;
@@ -71,7 +71,7 @@ const evenOddArrays = arr => {
     let evenArray = [];
     let oddArray = [];
     for (let i=0; i<arr.length; i++) {
-        if (arr[i] % 2 == 0) {
+        if (arr[i]%2 == 0) {
             evenArray.push(arr[i]);
         } else {
             oddArray.push(arr[i]);
@@ -93,7 +93,7 @@ const evenArrayDoLoop = arr => {
     let evenArray = [];
     let i = 0;
     do {
-        if (arr[i] % 2 == 0) {
+        if (arr[i]%2 == 0) {
             evenArray.push(arr[i]);
         }
         i++;
@@ -103,12 +103,12 @@ const evenArrayDoLoop = arr => {
 }
 console.log(evenArrayDoLoop(arr2));
 
-//****** Do Loop - Odd Array
+/******* Do Loop - Odd Array ********/
  const oddArrayDoLoop = arr => {
     let oddArray = [];
     let i = 0;
     do {
-        if (arr[i] % 2 != 0) {
+        if (arr[i]%2 != 0) {
             oddArray.push(arr[i]);
         }
         i++;
@@ -117,3 +117,87 @@ console.log(evenArrayDoLoop(arr2));
     return oddArray;
 }
  console.log(oddArrayDoLoop(arr2));
+
+var arr3 = [
+    {type: "socks", quantity: 1},
+    {type: "shoes", quantity: 3},
+    {type: "pants", quantity: 1},
+    {type: "socks", quantity: 5},
+    {type: "shoes", quantity: 1},
+    {type: "socks", quantity: 10}
+];
+
+/******* Write a function that returns the total quantity of the socks ****/
+const totalSocks = arr => {
+    let socks = 0;
+    for (let i=0; i<arr.length; i++){
+        socks += arr[i].quantity;
+    }
+    return socks;
+}
+console.log("Total socks " +totalSocks(arr3));
+
+/***** Using forEach *****/
+let totSocks = 0;
+
+arr3.forEach((addSocks) => {
+    totSocks += addSocks.quantity;
+    console.log(totSocks);
+}); //Can we return the final count of socks???
+// let i=0 or i = 0 ????
+
+var str = "supercalifragilisticexpialidocious";
+
+/*** Write a function that returns the number of "a" in the str ***/
+const retACount = str => {
+    let aCount=0;
+    for (let i=0; i<str.length; i++){
+        if(str.charAt(i)==='a'){
+            aCount += 1;
+        }
+    }
+    return aCount;
+}
+console.log("A Count in For loop is " + retACount(str));
+
+/*** Do Loop for returning 'A' letters count ***/
+const retACountDoLoop = str => {
+    let aCount=0;
+    let i=0;
+    do {
+        if(str.charAt(i)==='a'){
+            aCount += 1;
+        }
+        i++;
+    }
+    while (i<str.length);
+    return aCount;
+}
+console.log("A Count in Do loop is " + retACountDoLoop(str));
+
+/*** Write a function that returns the number of vowels in the str ***/
+const vowelCount = str => {
+    let vowel=0;
+    for (let i=0; i<str.length; i++){
+        if(`aeiou`.includes(str.charAt(i))){
+            vowel += 1;
+        }
+    }
+    return vowel;
+}
+console.log("Vowel Count in For loop is " +vowelCount(str));
+
+/*** Do loop for vowel count ***/
+const vowelCountDoLoop = str => {
+    let vowel=0;
+    let i=0;
+    do{
+        if(`aeiou`.includes(str.charAt(i))){
+            vowel += 1;
+        }
+        i++;
+    }
+    while(i<str.length);
+    return vowel;
+}
+console.log("Vowel Count in Do Loop is " +vowelCountDoLoop(str));
