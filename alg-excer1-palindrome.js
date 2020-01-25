55a6cb834eb70fb5c338f29879e3131d572ce32c
Suresh Paruchuri <sparuch@walmartlabs.com>
2020-01-25 15:04:02 -0800

    updated as per comments

--- alg-excer1-palindrome.js
+++ alg-excer1-palindrome.js
@@ -1,8 +1,8 @@
 function palindrome(input) {
-  str =  input.toString().toLowerCase();
-  var strLength= str.length;
+  str = input.toString().toLowerCase();
+  var strLength = str.length;
 
-  for (var i=0; i<strLength/2; i++) {
+  for (var i = 0; i < strLength/2; i++) {
     if (str[i] !== str[strLength-1-i]) {
       return input + "- is not Palindrome";
     }
@@ -12,4 +12,4 @@ function palindrome(input) {
 
 console.log(palindrome("SA as"));
 
-console.log(palindrome("345543"));
+console.log(palindrome(345543));
