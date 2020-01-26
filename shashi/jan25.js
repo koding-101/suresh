9ee84e78d9e67c4371c8fd20e704f088bd35fb38
Shashi Tirumala <s2tirum@homeoffice.wal-mart.com>
2020-01-25 22:47:07 -0500

    palindrome check

---
+++ shashi/jan25.js
@@ -0,0 +1,23 @@
+//Palindrome check
+
+str = "Eva, can I see bees in a cave?"
+
+const palindrome = str => {
+	let strNew;
+	if(typeof str === "number"){
+  	strNew = str.toString();
+  }
+   strNew = str.trim().replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+   
+   console.log("strNew", strNew);
+  
+  let strLen = Math.floor(strNew.length-1/2);
+	for(let i=0; i<strLen; i++){
+  	if(strNew[i] !== strNew[strNew.length-1 - i]){
+    	return false
+    }
+  }
+  return true;
+}
+
+console.log(palindrome(str));
