32965f7e1c493264f364a4d03f14768f450068fb
Shashi Tirumala <s2tirum@homeoffice.wal-mart.com>
2020-01-26 22:48:39 -0500

    Fix the function and remove regex

--- shashi/jan25.js
+++ shashi/jan25.js
@@ -1,15 +1,13 @@
-//Palindrome check
-
-str = "Eva, can I see bees in a cave?"
 
 const isPalindrome = str => {
   let strNew;
   if (typeof str === "number") {
     strNew = str.toString();
   }
-  strNew = str.trim().replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-
-  console.log("strNew", strNew);
+  if (str.length === 1 || str.length === "" || str === " ") {
+    return -1;
+  }
+  strNew = (str.split(" ")).join("").toLowerCase();
 
   let strLen = Math.floor(strNew.length - 1 / 2);
   for (let i = 0; i < strLen; i++) {
@@ -20,4 +18,31 @@ const isPalindrome = str => {
   return true;
 }
 
-console.log(isPalindrome(str));
+//object with Palindrome data
+var palindromeData = {
+  "Repaper": true,
+  "Red rum sir is murder": true,
+  "madam": true,
+  1234567654321: true,
+  12341234: false,
+  "not a plaindrome": false,
+  "a": -1,
+  1: -1,
+  " ": -1
+};
+
+//function that checks if given string is palindrome or not
+const verifyPalindrome = (fn, obj) => {
+  for (let str in obj) {
+    console.log("String is", str);
+    let palindromeCheck = fn(str);
+    console.log("palindromeCheck is", palindromeCheck);
+    if (palindromeCheck == obj[str]) {
+      console.log("palindrome check success");
+    } else {
+      console.log("palindrome check failed");
+    }
+  }
+}
+
+verifyPalindrome(isPalindrome, palindromeData);
