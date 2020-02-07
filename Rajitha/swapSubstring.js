e5e22058d395e5e144eb1350ed6a5ce299547763
Rajitha Tupakula <rtupaku>
2020-02-06 22:46:44 -0800

    Formatting

--- Rajitha/swapSubstring.js
+++ Rajitha/swapSubstring.js
@@ -11,38 +11,38 @@
  * Keep leftover strings in the same order.
  ****************************************************************************/
 const swapTwo = str => {
-      let strLength = str.length;  
-      let iterate = Math.trunc(strLength / 4);
-      let leftOver = strLength % 4;
-      let tempStr;
-      let outputStr = "";
-    
-      for (let i = 0; i < iterate; i++){
-        tempStr = str.substr(0,4);
-        str = str.substr(4);
-        outputStr += tempStr.substr(2) + tempStr.substr(0,2);
-      }
-    
-      if (leftOver > 0) {
-        outputStr += str.substr(strLength-strLength*4);
-      }
-      return outputStr;
-    }
-    
-    const testSwapTwo = (str, expected) => {
-      if (swapTwo(str) === expected) {
-        console.log("Pass");
-      }
-      else {
-        console.log("Fail");
-      }
-    }
-    
-    testSwapTwo("ABC", "ABC");
-    testSwapTwo("ABCD", "CDAB");
-    testSwapTwo("ABCDEFGH", "CDABGHEF");
-    testSwapTwo("munchkins", "ncmuinhks");
-    testSwapTwo("AABBCCDDEEFF", "BBAADDCCFFEE");
-    testSwapTwo("FFGGHHI", "GGFFHHI");
-    testSwapTwo("This is kilo weight", "isThs  ilokiei wght");
+  let strLength = str.length;  
+  let iterate = Math.trunc(strLength / 4);
+  let leftOver = strLength % 4;
+  let tempStr;
+  let outputStr = "";
+
+  for (let i = 0; i < iterate; i++){
+    tempStr = str.substr(0,4);
+    str = str.substr(4);
+    outputStr += tempStr.substr(2) + tempStr.substr(0,2);
+  }
+
+  if (leftOver > 0) {
+    outputStr += str.substr(strLength-strLength*4);
+  }
+  return outputStr;
+}
+
+const testSwapTwo = (str, expected) => {
+  if (swapTwo(str) === expected) {
+    console.log("Pass");
+  }
+  else {
+    console.log("Fail");
+  }
+}
+
+testSwapTwo("ABC", "ABC");
+testSwapTwo("ABCD", "CDAB");
+testSwapTwo("ABCDEFGH", "CDABGHEF");
+testSwapTwo("munchkins", "ncmuinhks");
+testSwapTwo("AABBCCDDEEFF", "BBAADDCCFFEE");
+testSwapTwo("FFGGHHI", "GGFFHHI");
+testSwapTwo("This is kilo weight", "isThs  ilokiei wght");
     
