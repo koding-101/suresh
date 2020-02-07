05d49b1b956a6a567e81bd3bd8444fd1abccaa06
Rajitha Tupakula <rtupaku>
2020-02-06 21:18:23 -0800

    modified as per review comments

--- Rajitha/itemPrice.js
+++ Rajitha/itemPrice.js
@@ -4,33 +4,36 @@
  * object has a product, a quantity and a price,
  ***********************************************************************/
 const getItemPrice = arr => {
-  let iArr = arr;
   let itemPrice = 0;
  
-  for (let i = 0; i < iArr.length; i++){
-    itemPrice += Number(iArr[i].price) * (iArr[i].quantity);
+  for (let i = 0; i < arr.length; i++){
+    itemPrice += arr[i].price * arr[i].quantity;
   }
   return itemPrice
 }
 
 const testGetItemPrice = (arr,expected) => {
-  return(getItemPrice(arr) === expected) ? true : false
+  if (getItemPrice(arr) === expected) {
+    console.log("PASS 👍");
+  } else {
+    console.log("Fail 😢");
+  }
 }
 
-console.log(testGetItemPrice([{product: "Milk", quantity: 1, price: 1.50}], 1.5));
-console.log(testGetItemPrice([{product: "Milk", quantity: 2, price: 1.50}], 3));
-console.log(testGetItemPrice([
+testGetItemPrice([{product: "Milk", quantity: 1, price: 1.50}], 1.5);
+testGetItemPrice([{product: "Milk", quantity: 2, price: 1.50}], 3);
+testGetItemPrice([
     { product: "Milk", quantity: 1, price: 1.50 },
     { product: "Cereals", quantity: 1, price: 2.50 }
-  ], 4))
-console.log(testGetItemPrice([
+  ], 4);
+testGetItemPrice([
     { product: "Milk", quantity: 1, price: 1.50 },
     { product: "Cereals", quantity: 1, price: 2.50 },
     { product: "Milk", quantity: 1, price: " " },
     { product: "Milk", quantity: 1, price: 9 }
-  ], 13))
-  console.log(testGetItemPrice([
+  ], 13);
+testGetItemPrice([
     { product: "Milk", quantity: 0, price: 1.50 },
     { product: "Cereals", quantity: 3, price: 2.50 }
-  ], 7.5))
+  ], 7.5);
 
